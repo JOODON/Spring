c4fb01dd9c87a70fd7955aada5e8ce9a2548fcf5
주동호 <100278301+JOODON@users.noreply.github.com>
2022-09-14 23:08:10 +0900

    스프링 프레임워크 pom.xml부터 다시 해보겠습니당..

--- src/main/java/DAO/Roledao.java
+++ src/main/java/DAO/Roledao.java
@@ -3,7 +3,11 @@ package DAO;
 import DTO.Role;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import static DAO.DAOSQL.*;
 
@@ -14,15 +18,22 @@ import java.util.List;
 
 @Repository //저장소라는객체를 선언해주는 느낌으로 생각하자!
 public class Roledao {
-    private NamedParameterJdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate jdbcTemplate;//JDBC 템플릿을 활용하기 위한 객체!
+    private SimpleJdbcInsert simpleJdbcInsert;//Insert문 활용하기 위한 객체라고 생각하면 될듯! 
     private RowMapper<Role> rowMapper = BeanPropertyRowMapper.newInstance(Role.class);
 
     public Roledao (DataSource dataSource){
         this.jdbcTemplate=new NamedParameterJdbcTemplate(dataSource);
+        this.simpleJdbcInsert =new SimpleJdbcInsert(dataSource).withTableName("role");
+        //어떤 테이블을 사용할 것인가라고 이름을 지정해준다
     }
     public List<Role> selectAll(){
         return jdbcTemplate.query(SELECT_ALL, Collections.emptyMap(),rowMapper);
                 //쿼리문,비어있는 맵 객체
+    }
+    public int insert(Role role){
+        SqlParameterSourceSource params = new BeanPropertySqlParameterSource(role);
+        return simpleJdbcInsert.execute(parameters);
     }
 
 }
